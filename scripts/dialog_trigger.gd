10b1e2e8a97c23eb4855b9523beb52626b2a0db8
Lance <dogmandevices@gmail.com>
2024-12-30 18:19:55 -0500

    bug test and fix level 3 and dialog system; make it so Mila doubles HP

--- scripts/dialog_trigger.gd
+++ scripts/dialog_trigger.gd
@@ -15,29 +15,29 @@ var dialog_1 = [
 	"Mila: Yes, and thorny, diseased vines are spreading. I would advise against continuing up the tree from outside.",
 	"Cedar: But...",
 	"Mila: The thorns are too thick here where the shade begins, but they thin-out above.",
-	"Cedar: I saw the passage inside was blocked by thorns. If I can find a way around them, I should be able to climb.",
-	"Mila: Yes. Be careful. There is something very strange about all of this..."
+	"Cedar: I saw the passage inside was blocked by thorns.",
+	"If I can find a way around them, I should be able to climb.",
+	"Mila: Yes. Be careful. There is something very strange about all of this...",
+	"Cedar: ...I should get going.",
+	"Mila: Take this robe of my thread.",
+	"It is lightweight and nearly invisible, but it should provide you with much more protection."
 	]
-
-#func _process(delta: float) -> void:
-	#if $CollisionShape2D/DialogBox.visible == true:
-		#Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	#elif $CollisionShape2D/DialogBox.visible == false:
-		#Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		await get_tree().create_timer(0.2).timeout
 		get_tree().paused = true
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-		#body.ui_state = body.UIState.MENU
-		$CollisionShape2D/DialogBox.visible = true
+		$".".visible = true
 		
 		match topic:
 			Topics.LVL3MILA:
 				conversation = dialog_1
+				body.max_health = 6
+				body.health = 6
+				body.update_health.emit(6, 6)
 		
-		$CollisionShape2D/DialogBox/Textbox/Label.text = conversation[0]
+		$DialogBox/Textbox/Label.text = conversation[0]
 		counter += 1
 
 func _on_next_button_pressed() -> void:
@@ -49,11 +49,11 @@ func _on_next_button_pressed() -> void:
 	print(counter)
 	if counter >= size:
 		counter = 0
-		$CollisionShape2D/DialogBox.visible = false
+		$".".visible = false
 		Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 		get_tree().paused = false
 	else:
-		$CollisionShape2D/DialogBox/Textbox/Label.text = conversation[counter]
+		$DialogBox/Textbox/Label.text = conversation[counter]
 		print("conversation[counter]")
 		print(conversation[counter])
 		counter += 1
